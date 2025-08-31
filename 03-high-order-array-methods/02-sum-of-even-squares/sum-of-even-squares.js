97533d446ae3d002d5abbb1d9e76f1bb63e84331
wasif-technyx <142510796+wasif-technyx@users.noreply.github.com>
2025-08-31 14:33:11 +0500

    Third Module WIP

--- 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
+++ 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
@@ -1,3 +1,14 @@
-function sumOfEvenSquares() {}
+function sumOfEvenSquares(arr) {
+    const getAllEvenNumbers = arr.filter(ar => ar % 2 == 0)
+        const sumOfSquaredNumber = getAllEvenNumbers.reduce((acc, curr) => {
+            const squareOfNumber = curr * curr
+
+            acc += squareOfNumber
+            return acc
+        },0)
+
+        return sumOfSquaredNumber
+
+}
 
 module.exports = sumOfEvenSquares;
--- 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
+++ 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
@@ -1,3 +1,20 @@
-function calculateTotalSalesWithTax() {}
+function calculateTotalSalesWithTax(productsArray, taxPercentage) {
+    const totalAmount = productsArray.reduce((acc, curr) => {
+        // console.log(curr)
+        const priceOfProduct = curr.price
+        const quantityOfProduct = curr.quantity
+
+        const totalPriceWithoutTax = priceOfProduct * quantityOfProduct
+        acc += totalPriceWithoutTax
+
+        return acc
+    }, 0)
+
+    const calculateTaxAmount = (totalAmount / 100) * taxPercentage
+
+    const totalPriceWithTax = (calculateTaxAmount + totalAmount)
+
+    return totalPriceWithTax
+}
 
 module.exports = calculateTotalSalesWithTax;

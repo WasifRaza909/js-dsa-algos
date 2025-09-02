252a9bad8bfc2b77ca442393ddd3916f35b39eb5
wasif-technyx <142510796+wasif-technyx@users.noreply.github.com>
2025-09-02 23:58:40 +0500

    Completed High Order array methods

--- 03-high-order-array-methods/09-password-validator/password-validator.js
+++ 03-high-order-array-methods/09-password-validator/password-validator.js
@@ -1,3 +1,29 @@
-function validatePassword() {}
+function validatePassword(string) {
+    const strToArr = string.split('')
+
+    if (string.length < 8) {
+        return false
+    }
+
+    const hasUpperCaseLetter = strToArr.some(str => isNaN(str) && str == str.toUpperCase())
+    
+    if (!hasUpperCaseLetter) {
+        return false
+    }
+
+    const hasLowerCaseLetter = strToArr.some(str => isNaN(str) && str == str.toLowerCase())
+
+    if (!hasLowerCaseLetter) {
+        return false
+    }
+
+    const hasDigit = strToArr.some(str => !isNaN(str))
+
+    if (!hasDigit) {
+        return false
+    }
+
+    return true
+}
 
 module.exports = validatePassword;
--- 03-high-order-array-methods/10-find-missing-letter-refactor/find-missing-letter-refactor.js
+++ 03-high-order-array-methods/10-find-missing-letter-refactor/find-missing-letter-refactor.js
@@ -1,3 +1,14 @@
-function findMissingLetter() {}
+function findMissingLetter(array) {
+    const alphabets = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+    const getIndexOfFirstAlphabet = alphabets.indexOf(array[0])
+    const getIndexOflastAlphabet = alphabets.indexOf(array[array.length - 1])
+    
+    const sliceAlphabets = alphabets.slice(getIndexOfFirstAlphabet, getIndexOflastAlphabet + 1).split('')
+
+    const missingAlphabet = sliceAlphabets.find(ar => !array.includes(ar))
+
+    return missingAlphabet
+}
 
 module.exports = findMissingLetter;
